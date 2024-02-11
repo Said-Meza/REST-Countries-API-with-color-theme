3a4a46d58586f07e0d062ff8e6b34830d870822f
saidmeza <176p0393@itspozarica.edu.mx>
2024-02-10 20:31:15 -0600

    version 2.0.0

--- app.js
+++ app.js
@@ -1,62 +1,57 @@
 import api from './js/api_routes.js';
-import apigetpagination from "./api.js";
+import apigetpagination from "./js/getAll.js";
 import darktheme from "./js/darktheme.js";
-import apibuscar from "./js/apisearch.js";
+import ajax from './js/ajax.js';
 
-//VARIABLES DEL DOM
+    //VARIABLES DEL DOM
     const d=document,
         w=window,
         $template=d.getElementById("card").content,
         $fragment=d.createDocumentFragment(),
         $cards=d.querySelector(".cards"),
-        $search=d.querySelector(".search__form"),
         ls=localStorage;
         
         // console.log($template)
-        darktheme("dark-mode-btn","dark-mode-background","dark-mode-element");
+        darktheme(".nav__darktext","dark-mode-background","dark-mode-element");
        
-       
-            const paises = await apigetpagination(api);
-    
-            
-            paises.forEach(element => {
-                // console.log(element.name.common)
+             await apigetpagination({
+                url:api.GETALL,
+                api:api,
+                cbSuccess:(paises)=>{
 
-                $template.querySelector(".card__img").src=element.flags.svg
-                $template.querySelector(".card__title").textContent=element.name.common
-                $template.querySelector(".card__populacion").textContent=element.population
-                $template.querySelector(".card__Region").textContent=element.region
-                $template.querySelector(".card__capital").textContent=element.capital
-                $template.querySelector(".card__link").href=`./views/more.html#/query=${element.name.common}`;
-
-                if (ls.getItem("theme") === null) {
-                    ls.setItem("theme","light")
-                    $template.querySelector("article").classList.removed(`dark-mode-element`);
-                    $template.querySelector(".card__link").classList.removed(`dark-mode-element`);
+                    paises.forEach(element => {
                         
+                        $template.querySelector(".card__img").src=element.flags.svg
+                        $template.querySelector(".card__title").textContent=element.name.common
+                        $template.querySelector(".card__populacion").textContent=element.population
+                        $template.querySelector(".card__Region").textContent=element.region
+                        $template.querySelector(".card__capital").textContent=element.capital
+                        $template.querySelector(".card__link").href=`./views/more.html#/query=${element.name.common}`;
+        
+                        if (ls.getItem("theme") === null) {
+                            ls.setItem("theme","light")
+                            $template.querySelector("article").classList.removed(`dark-mode-element`);
+                            $template.querySelector(".card__link").classList.removed(`dark-mode-element`);
+                                
+                        }
+                        
+                        if (ls.getItem("theme") === "dark") {
+                            $template.querySelector("article").classList=`dark-mode-element`;
+                            $template.querySelector(".card__link").classList.add(`dark-mode-element`);
+                           
+                        }
+        
+                        let $clone = document.importNode($template, true);
+                        $fragment.appendChild($clone);
+                    });
+                    $cards.appendChild($fragment);
                 }
-                
-                if (ls.getItem("theme") === "dark") {
-                    $template.querySelector("article").classList=`dark-mode-element`;
-                    $template.querySelector(".card__link").classList.add(`dark-mode-element`);
-                   
-                }
-
-                let $clone = document.importNode($template, true);
-                $fragment.appendChild($clone);
             });
-
-
-            $cards.appendChild($fragment);
-
-            darktheme("dark-mode-btn","dark-mode-background","dark-mode-element");
-       
-
+    
+        darktheme(".nav__darktext","dark-mode-background","dark-mode-element");
           
             d.addEventListener("submit", async (e)=>{
                 e.preventDefault();
-
-                console.log(e.target)
 
                 if(e.target.matches(".search__form")){
 
@@ -65,36 +60,27 @@ import apibuscar from "./js/apisearch.js";
 
                     if (!forminput==0 || !forminput=="" ) 
                     {
-                        // console.log(forminput)
-
-                        let url=`${api.SEARCH}${forminput}`
-                        // console.log(url)
-
-                        const busqueda = await apibuscar(url);
-                        
-                        busqueda.forEach(element => {
-                            console.log(element)
-                            
-                            $template.querySelector(".card__img").src=element.flags.svg
-                            $template.querySelector(".card__title").textContent=element.name.common
-                            $template.querySelector(".card__populacion").textContent=element.population
-                            $template.querySelector(".card__Region").textContent=element.region
-                            $template.querySelector(".card__capital").textContent=element.capital
-                            $template.querySelector(".card__link").href=`./views/more.html#/query=${element.name.common}`;
-                            let $clone = document.importNode($template, true);
-                            $fragment.appendChild($clone);
-                    
+                        await ajax({
+                            url:`${api.SEARCH}${forminput}`,
+                            cbSuccess:(busqueda)=>{
+                                    busqueda.forEach(element => {
+                                    
+                                    $template.querySelector(".card__img").src=element.flags.svg
+                                    $template.querySelector(".card__title").textContent=element.name.common
+                                    $template.querySelector(".card__populacion").textContent=element.population
+                                    $template.querySelector(".card__Region").textContent=element.region
+                                    $template.querySelector(".card__capital").textContent=element.capital
+                                    $template.querySelector(".card__link").href=`./views/more.html#/query=${element.name.common}`;
+                                    let $clone = document.importNode($template, true);
+                                    $fragment.appendChild($clone);
+                                })
+                                $cards.innerHTML="";
+                                $cards.appendChild($fragment);
+                            }
                         })
-
-                        $cards.innerHTML="";
-                        $cards.appendChild($fragment);
+                        
                     }
-  
                 }
-
-                
-
-
             })
 
             d.addEventListener("change",async e=>{
@@ -105,67 +91,62 @@ import apibuscar from "./js/apisearch.js";
                     let url=`${api.REGION}${region}`;
                     // console.log(url)
 
-                    const side = await apibuscar(url);
-                        
-                    side.forEach(element => {
-                        console.log(element)
-                        
-                        $template.querySelector(".card__img").src=element.flags.svg
-                        $template.querySelector(".card__title").textContent=element.name.common
-                        $template.querySelector(".card__populacion").textContent=element.population
-                        $template.querySelector(".card__Region").textContent=element.region
-                        $template.querySelector(".card__capital").textContent=element.capital
-                        $template.querySelector(".card__link").href=`./views/more.html#/query=${element.name.common}`;
-                        let $clone = document.importNode($template, true);
-                        $fragment.appendChild($clone);
-                
-                    })
+                    // const side = await apibuscar(url);
+                        await ajax({
+                            url:`${api.REGION}${region}`,
+                            cbSuccess:(side)=>{
 
-                    $cards.innerHTML="";
-                    $cards.appendChild($fragment);
+                                side.forEach(element => {
+                                    
+                                    $template.querySelector(".card__img").src=element.flags.svg
+                                    $template.querySelector(".card__title").textContent=element.name.common
+                                    $template.querySelector(".card__populacion").textContent=element.population
+                                    $template.querySelector(".card__Region").textContent=element.region
+                                    $template.querySelector(".card__capital").textContent=element.capital
+                                    $template.querySelector(".card__link").href=`./views/more.html#/query=${element.name.common}`;
+                                    let $clone = document.importNode($template, true);
+                                    $fragment.appendChild($clone);
+                            
+                                })
+            
+                                $cards.innerHTML="";
+                                $cards.appendChild($fragment);
+                                
+                            }
+                        })
                     
                 }
 
             })
 
-
             w.addEventListener("scroll", async (e)=>{
                 let {scrollTop,clientHeight,scrollHeight}=d.documentElement;
 
-
                 if(scrollTop+clientHeight>=scrollHeight){
-                    // console.log("hola si entro al if")
+                    
                     api.PAGE++;
 
-                   const morepaises = await apigetpagination(api);
-
-                   morepaises.forEach(element => {
-                    // console.log(element)
-    
-                    $template.querySelector(".card__img").src=element.flags.svg
-                    $template.querySelector(".card__title").textContent=element.name.common
-                    $template.querySelector(".card__populacion").textContent=element.population
-                    $template.querySelector(".card__Region").textContent=element.region
-                    $template.querySelector(".card__capital").textContent=element.capital
-                    $template.querySelector(".card__link").href=`./views/more.html#/query=${element.name.common}`;
-    
-                    // if (ls.getItem("theme") === null) {
-                    //     ls.setItem("theme","light")
-                    //     $template.querySelector("article").classList.removed(`dark-mode-element`);
-                    //     $template.querySelector(".card__link").classList.removed(`dark-mode-element`);
-                            
-                    // }
-                    
-                    // if (ls.getItem("theme") === "dark") {
-                    //     $template.querySelector("article").classList=`dark-mode-element`;
-                    //     $template.querySelector(".card__link").classList.add(`dark-mode-element`);
-                       
-                    // }
-    
-                    let $clone = document.importNode($template, true);
-                    $fragment.appendChild($clone);
-                });
-                    $cards.appendChild($fragment);
+                   await apigetpagination({
+                    url:api.GETALL,
+                    api:api,
+                    cbSuccess:((morepaises)=>{
+                        morepaises.forEach(element => {
+                            // console.log(element)
+            
+                                $template.querySelector(".card__img").src=element.flags.svg
+                                $template.querySelector(".card__title").textContent=element.name.common
+                                $template.querySelector(".card__populacion").textContent=element.population
+                                $template.querySelector(".card__Region").textContent=element.region
+                                $template.querySelector(".card__capital").textContent=element.capital
+                                $template.querySelector(".card__link").href=`./views/more.html#/query=${element.name.common}`;
+                
+                
+                                let $clone = document.importNode($template, true);
+                                $fragment.appendChild($clone);
+                            });
+                            $cards.appendChild($fragment);
+                    })
+                   })
                 }
 
             })        
--- js/darktheme.js
+++ js/darktheme.js
@@ -1,52 +1,33 @@
 const d= document,
     ls=localStorage;
-//    let conta=0;
 
 // <!-- data-dark-background is to background -->
 // <!-- data-dark-element is to element -->
  
-//darktheme("dark-mode-btn","dark-mode-background","dark-mode-element");
     export default function darktheme(btn,classDarkBack,classDarkEl) {
 
         let conta=0;
         
-        const $themeDarkbtn= d.getElementById(btn),
-            $selectorsBackground = d.querySelectorAll("[data-dark-background]"),
-            $selectorsElements = d.querySelectorAll("[data-dark-element]");
+        const $selectorsBackground = d.querySelectorAll("[data-dark-background]"),
+                $selectorsElements = d.querySelectorAll("[data-dark-element]");
             
-            // console.log($themeDarkbtn)
-            // console.log($selectorsBackground)
-            // console.log($selectorsElements)
-            
-
         const darkMode = () =>
         {
             $selectorsBackground.forEach(el => el.classList.add(classDarkBack));
             $selectorsElements.forEach(el => el.classList.add(classDarkEl));
-            
-            ls.setItem("theme","dark")
-            // console.log(classDark)
-            
-            
+            ls.setItem("theme","dark") 
         }    
         const lighMode = () =>{
-            
             $selectorsBackground.forEach(el => el.classList.remove(classDarkBack));
             $selectorsElements.forEach(el => el.classList.remove(classDarkEl));
-            
             ls.setItem("theme","light")
-
         }    
     
-        
         d.addEventListener("click",(e)=>{
-            
-            // console.log(e.target)
-
-            if(e.target.matches(".nav__darktext"))
+           
+            if(e.target.matches(btn))
             {       
                 conta++;
-                // console.log(`---`,conta)
                 if(conta === 1){
                     darkMode();
                     conta=1;
@@ -59,33 +40,22 @@ const d= document,
 
             }
         })
-    
        
         d.addEventListener("DOMContentLoaded",(e)=>{
-            // console.log(`este es el domcontentload`)
-            // alert("hola mundo desde tema oscuro")
-
+           
                 if (ls.getItem("theme") === null) {
                     ls.setItem("theme","light")
                     let conta=0;
-                    // console.log("hay variable para themedark")
                 }
-                // else{
-                //     console.log("no hay variable en localstores")
-                // }
-        
+                
                 if (ls.getItem("theme") === "light") {
                     lighMode();
                     conta=0;
-                    // console.log("tema claro")
                 }
-                
                 if (ls.getItem("theme") === "dark") {
                     darkMode();
                     conta=1;
-                    // console.log("tema obscuro")
+                    
                 }
-
         })
-    
     };
